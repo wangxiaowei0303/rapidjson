3be62406af84c2c879f43fc2e5e8df41c53b6ae5
miloyip@gmail.com <miloyip@gmail.com@c5894555-1306-4e8d-425f-1f6f381ee07c>
2012-02-28 06:05:11 +0000

    Fixed issue 17:	gcc complains about potentially uninitialized variable use

--- branches/version0.1x/include/rapidjson/reader.h
+++ branches/version0.1x/include/rapidjson/reader.h
@@ -512,7 +512,7 @@ private:
 		}
 
 		// Parse 64bit int
-		uint64_t i64;
+		uint64_t i64 = 0;
 		bool useDouble = false;
 		if (try64bit) {
 			i64 = i;
@@ -537,7 +537,7 @@ private:
 		}
 
 		// Force double for big integer
-		double d;
+		double d = 0.0;
 		if (useDouble) {
 			d = (double)i64;
 			while (s.Peek() >= '0' && s.Peek() <= '9') {
